c4e70c3294cf846c746bf350a474aaef20307ed5
Ashfaq Rehman <rehman@sheridancollege.ca>
2019-01-26 21:04:36 -0500

    added atm.py

---
+++ atm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+print("Hello!")
+trys=1
+pin="1234"
+while True: 
+    user_input = input("Please enter your PIN: ")    
+    if trys == 3:
+        print("Account locked. The police is on its way.")
+        break
+    elif user_input == pin:
+        print("Your account balance is: 0. Goodbye!")
+        break
+    else:
+        trys += 1
+        
